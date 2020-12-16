249dd136d51abc39eb932b02811f9daf2c3bd45d
Ville Häyrynen <hayrynenville@outlook.com>
2020-12-16 17:50:07 +0200

    Herkoko commit

--- db.js
+++ db.js
@@ -3,10 +3,10 @@ let pool = null;
 try {
   pool  = mysql.createPool({
     connectionLimit : 10,
-    host            : 'localhost',
-    user            : 'devuser',
-    password        : '123',
-    database        : 'webappdb'
+    host            : 'ec2-54-158-222-248.compute-1.amazonaws.com',
+    user            : 'hpeclwdwigmbwg',
+    password        : 'c668f36867b76b66ca423538e85edf1f254efdaf029cc91e36bf9c6d4cc248f8',
+    database        : 'd33pnrec03c9d8'
   });
 
 } catch (error) {
